21dc671a4f1ef693d6c4dbc90ae56a6bd0d01422
TASANAI PINGMUEANG <tasanaikq@gmail.com>
2018-05-09 02:31:01 +0700

    Add paging helper class

---
+++ RegSystem.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegSystem.WebUI.HtmlHelpers
+{
+    public class PagingHelpers
+    {
+    }
+}
