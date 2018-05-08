698b135a5b0282ffb0bc7da69a354dba205094f8
Thanyalak Surinpun <thanyalak_su@cmu.ac.th>
2018-05-09 02:04:12 +0700

    Modified routeConfig for pagination

--- RegSystem.WebUI/App_Start/RouteConfig.cs
+++ RegSystem.WebUI/App_Start/RouteConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +12,12 @@ namespace RegSystem.WebUI
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { Controller = "Course", action = "List" }
+            );
 
             routes.MapRoute(
                 name: "Default",
