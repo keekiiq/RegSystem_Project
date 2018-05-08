23113dba98de1df79abc376cdfd369d1307ae339
TASANAI PINGMUEANG <tasanaikq@gmail.com>
2018-05-09 02:47:13 +0700

    Edit page style

--- RegSystem.WebUI/Controllers/CourseController.cs
+++ RegSystem.WebUI/Controllers/CourseController.cs
@@ -6,20 +6,35 @@ using Moq;
 using Ninject;
 using RegSystem.Domain.Entities;
 using RegSystem.Domain.Abstract;
+using RegSystem.WebUI.Models;
 
 namespace RegSystem.WebUI.Controllers
 {
     public class CourseController : Controller
     {
         private ICourseRepository repository;
+        public int PageSize = 4;
 
         public CourseController(ICourseRepository courseRepository)
         {
             this.repository = courseRepository;
         }
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Courses);
+            CoursesListViewModel model = new CoursesListViewModel
+            {
+                Courses = repository.Courses
+                .OrderBy(p => p.CourseID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Courses.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
