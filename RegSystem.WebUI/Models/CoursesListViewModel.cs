2c5a134938ec2bcf89e04fc631461067f9586d64
TASANAI PINGMUEANG <tasanaikq@gmail.com>
2018-05-09 02:38:15 +0700

    Add Course List View Model

---
+++ RegSystem.WebUI/Models/CoursesListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using RegSystem.Domain.Entities;
+namespace RegSystem.WebUI.Models
+{
+    public class CoursesListViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
